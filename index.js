4e5b817c0f603a5648ac1ce8399dc7626cb59a59
unknown <sayedpritom999@gmail.com>
2022-05-13 02:32:13 +0600

    created server, api for read and update items

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6jazw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const itemsCollection = client.db("warehouse").collection("items");
+
+        // Items API 
+
+        // get 6 inventory items for home page
+        app.get('/items', async (req, res) => {
+            const query = {};
+            const cursor = itemsCollection.find(query).limit(6);
+            const items = await cursor.toArray();
+            res.send(items)
+        })
+
+        // get a particular item
+        app.get('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await itemsCollection.findOne(query);
+            res.send(item)
+        })
+
+        // update
+        app.put('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedItem = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+
+            const updateDoc = {
+                $set: {
+                    name: updatedItem.name,
+                    price: updatedItem.price,
+                    description: updatedItem.description,
+                    img: updatedItem.img,
+                    supplier: updatedItem.supplier,
+                    quantity: updatedItem.quantity,
+                },
+            };
+            const result = await itemsCollection.updateOne(filter, updateDoc, options);
+            res.send(result)
+        })
+
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+
+
+app.listen(port, () => {
+    console.log('listening on port ' + port)
+})
